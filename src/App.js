e2560411cfdd0cc1d8a2016619cd90c488bb41f4
akunumanarif <numanarif87@gmail.com>
2021-11-29 00:56:07 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './app.css';
 import Home from './pages/home/Home';
 import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
-// const { default: Sidebar } = require('./components/sidebar/Sidebar');
-// const { default: Topbar } = require('./components/topbar/Topbar');
 
 function App() {
 	return (
