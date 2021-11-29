e9624ab81f727ab941faf4a10b727a3c43f61f2e
akunumanarif <numanarif87@gmail.com>
2021-11-29 22:18:44 +0700

    make widget

--- src/components/charts/Chart.jsx
+++ src/components/charts/Chart.jsx
@@ -1,66 +1,17 @@
 import './chart.css';
-import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 
-export default function Chart() {
-	const data = [
-		{
-			name: 'Jan',
-			'Active User': 4000
-		},
-		{
-			name: 'Feb',
-			'Active User': 2000
-		},
-		{
-			name: 'Mar',
-			'Active User': 1000
-		},
-		{
-			name: 'Apr',
-			'Active User': 1000
-		},
-		{
-			name: 'Mei',
-			'Active User': 2000
-		},
-		{
-			name: 'Jun',
-			'Active User': 1000
-		},
-		{
-			name: 'Jul',
-			'Active User': 4000
-		},
-		{
-			name: 'Aug',
-			'Active User': 3000
-		},
-		{
-			name: 'Sep',
-			'Active User': 3000
-		},
-		{
-			name: 'Oct',
-			'Active User': 4000
-		},
-		{
-			name: 'Nov',
-			'Active User': 6000
-		},
-		{
-			name: 'Des',
-			'Active User': 8000
-		}
-	];
+export default function Chart({ title, data, dataKey, grid }) {
 	return (
 		<div className="chart">
-			<h3 className="chartTitle">User Analitycs</h3>
+			<h3 className="chartTitle">{title}</h3>
 			<ResponsiveContainer width="100%" aspect={4 / 1}>
 				<LineChart data={data}>
 					<XAxis dataKey="name" stroke="#5550bd" />
-					<Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+					<Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
 					<Tooltip />
-					<CartesianGrid sstroke="#ccccce" strokeDasharray="5 5" />
+					{grid && <CartesianGrid sstroke="#ccccce" strokeDasharray="5 5" />}
+					<Legend />
 				</LineChart>
 			</ResponsiveContainer>
 		</div>
---
+++ src/components/widgets/widgetLarge/WidgetLarge.jsx
@@ -0,0 +1,5 @@
+import './widgetLarge.css';
+
+export default function WidgetLarge() {
+	return <div className="widgetLarge">Large</div>;
+}
