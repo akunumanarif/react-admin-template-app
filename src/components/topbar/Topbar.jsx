fbb237bb9c396bb6982cc4d74536e57d34c031ff
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-17 21:34:58 +0700

    fix topbar get username image issue

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,32 +1,36 @@
-import './topbar.css';
-import { LanguageOutlined, NotificationsNone, Settings } from '@material-ui/icons';
+import "./topbar.css";
+import {
+  LanguageOutlined,
+  NotificationsNone,
+  Settings,
+} from "@material-ui/icons";
 
 export default function Topbar() {
-	return (
-		<div className="topbar">
-			<div className="topbarWrapper">
-				<div className="topLeft">
-					<span className="logo">My Admin</span>
-				</div>
-				<div className="topRight">
-					<div className="topbarIconContainer">
-						<NotificationsNone />
-						<span className="topbarIconBadge">2</span>
-					</div>
-					<div className="topbarIconContainer">
-						<LanguageOutlined />
-						{/* <span className="topbarIconBadge">2</span> */}
-					</div>
-					<div className="topbarIconContainer">
-						<Settings />
-					</div>
-					<img
-						src="https://lh3.googleusercontent.com/ogw/ADea4I5WEa9ZGn4x898aMqR8C53tGonv0ZR-oz5NHT5xZA=s83-c-mo"
-						alt=""
-						className="topAvatar"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">My Admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <LanguageOutlined />
+            {/* <span className="topbarIconBadge">2</span> */}
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
